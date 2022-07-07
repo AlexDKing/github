f13380140fd332b98b95d0944841461dc1001113
Alex king <1017527036@qq.com>
2022-07-07 21:48:57 +0800

    Initial Commit

---
+++ github/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  github
+//
+//  Created by Alex·King on 2022/7/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
